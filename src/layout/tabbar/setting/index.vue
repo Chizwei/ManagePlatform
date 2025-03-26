d47b6f11e49739147331678fb95b60e269d902b8
chizwei <chizwei@163.com>
2025-03-26 11:12:50 +0800

    add fullScreen

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tabbar_right">
     <el-button size="small" icon="Refresh" circle @click="updateData"></el-button>
-      <el-button size="small" icon="FullScreen" circle></el-button>
+      <el-button size="small" icon="FullScreen" circle @click="fullScreen"></el-button>
       <el-button size="small" icon="Setting" circle></el-button>
       <img src="../../../../public/logo.png" style="width: 24px;height: 24px;margin:0 10px 0 20px;  ">
       <el-dropdown>
@@ -29,6 +29,21 @@ let layoutSettingStore = useLayOutSettingStore()
 const updateData = ()=>{
   layoutSettingStore.refesh = !layoutSettingStore.refesh
 }
+
+
+const fullScreen = ()=>{
+  // dom对象的一个属性，用于判断是否全屏（全屏：true，否：false）
+  // 也有一些插件实现，使用时需要考虑兼容性问题
+  let full = document.fullscreenElement
+  if(!full){
+    // 根据文档根节点方法实现全屏
+    document.documentElement.requestFullscreen()
+  }else{
+    //退出全屏模式
+    document.exitFullscreen()
+  }
+}
+
 
 
 </script>
