5b8d822a2ba3db64846b71e2f6382dd8264c68cb
chizwei <chizwei@163.com>
2025-03-26 22:00:04 +0800

    change server

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,13 +1,12 @@
 // 创建用户相关的小仓库
 import { defineStore } from "pinia";
 // 引入登录接口
-import { reqLogin, reqUserInfo } from "../../api/user";
-import type { loginForm, loginResponseData } from "../../api/user/type";
+import { reqLogin, reqUserInfo, reqLogout } from "../../api/user";
+
 import type { UserState } from "../../store/modules/types"
 import { GET_TOKEN, REMOVE_TOKEN, SET_TOKEN } from "../../utils/token";
 // 引入路由（常量路由）
 import { constantRoute } from "../../router/routers";
-
 
 
 let useUserStore = defineStore('User', {
@@ -25,22 +24,12 @@ let useUserStore = defineStore('User', {
   //  异步|逻辑处理
   actions: {
     // 用户登录的方法
-    async userLogin(data: loginForm) {
-      let res: loginResponseData = await reqLogin(data)
-      if (res.code === 200) {
-        // 通过pinia仓库存储一下token
-        // pinia|vueX 存储数据都是利用js对象存储，不会持久拥有
-        this.token = res.data.token as string
-        // 还需要本地存储，持久化
-        SET_TOKEN(res.data.token as string)
-        // 保证当前async函数返回一个成功的promise
-        return 'ok'
+    async userLogin(data: any) {
 
-      } else {
-        // 这里的error是服务的error 和 axios中的error不一样
-        // axios中的是http网络的错误
-        return Promise.reject(new Error(res.data.message))
-      }
+      // 登录请求
+      let res = await reqLogin(data)
+      console.log(res);
+
     },
     // 获取用户信息
     async userInfo() {
